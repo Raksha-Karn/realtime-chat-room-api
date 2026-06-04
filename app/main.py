3557934d7f895908a12b53a90a53da02e825b748
Raksha Karn <karnraksha41@gmail.com>
2026-06-04 12:17:13 +0545

    chore(main): add frontend file path

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from app.routers import users, rooms, chat
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from contextlib import asynccontextmanager
+from pathlib import Path
 from app.pubsub import redis_listener
 import asyncio
 
@@ -14,6 +17,13 @@ app = FastAPI(title="Chat API", version="1.0.0", lifespan=lifespan)
 app.include_router(users.router)
 app.include_router(rooms.router)
 app.include_router(chat.router)
+
+static_dir = Path(__file__).resolve().parent / "static"
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+@app.get("/")
+def frontend():
+    return FileResponse(static_dir / "index.html")
 
 @app.get("/health")
 def health():
